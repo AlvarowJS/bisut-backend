184caa745233585a85c9cadd882ae3cc34c9182e
AlvarowJS <alvaruwiiu@hotmail.com>
2024-08-31 00:24:35 -0500

    correcion con el registro y actualizacion de proveedor

--- app/Http/Controllers/Api/V1/ProveedorController.php
+++ app/Http/Controllers/Api/V1/ProveedorController.php
@@ -21,11 +21,13 @@ class ProveedorController extends Controller
     {
         $userCurrent = auth()->id();
         $data = new Proveedor();
-        $data ->user_id = $userCurrent;
-        $data -> nombre = $request->nombre;
-        $data -> telefono = $request->telefono;
-        $data -> mail = $request->mail;
-        $data -> status = true;
+        $data->user_id = $userCurrent;
+        $data->nombre = $request->nombre;
+        $data->telefono = $request->telefono;
+        $data->direccion = $request->direccion;
+        $data->codigo_postal = $request->codigo_postal;
+        $data->mail = $request->mail;
+        $data->estado = true;
         $data->save();
         return response()->json($data);
     }
@@ -45,11 +47,15 @@ class ProveedorController extends Controller
     public function update(Request $request, string $id)
     {
         $data = Proveedor::find($id);
-        $data -> user_id = $request->user_id;
-        $data -> nombre = $request->nombre;
-        $data ->telefono = $request->telefono;
-        $data -> mail = $request->mail;
-        $data -> status = true;
+        $data->user_id = $request->user_id;
+        $data->nombre = $request->nombre;
+        $data->telefono = $request->telefono;
+        $data->direccion = $request->direccion;
+        $data->codigo_postal = $request->codigo_postal;
+        $data->mail = $request->mail;
+        $data->estado = $request->estado;
+        $data->save();
+        return response()->json($data);
     }
 
     /**
@@ -57,6 +63,11 @@ class ProveedorController extends Controller
      */
     public function destroy(string $id)
     {
-        
+        $data = Proveedor::find($id);
+        if (!$data) {
+            return response()->json(["message" => "not found"], 404);
+        }
+        $data->delete();
+        return response()->json($data);
     }
 }
